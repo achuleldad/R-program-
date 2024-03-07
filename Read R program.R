470ade45beea2db86cf522d4ea1c97ac2ae9ed6e
unknown <l00179049@atu.ie>
2024-03-07 11:19:43 +0000

    commit

---
+++ Read R program.R
@@ -0,0 +1,23 @@
+x <- c(3, 12, 7)
+x
+y <- c(4,9,11)
+z <- x+y
+z
+x <- x+2
+x
+y <- y + 40 
+y
+sum(y)
+mean(y)
+
+i <- 5 
+i
+is.integer(i)
+i <- 5L
+is.numeric(i)
+is.integer(i)
+x1<-4L
+x1
+class(x1)
+x2 <- 4
+class(x2)
