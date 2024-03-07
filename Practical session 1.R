1f7f82644a20b7c4f867fb60ef907b9e8df8b220
unknown <l00179049@atu.ie>
2024-03-07 12:22:10 +0000

    commit

---
+++ Practical session 1.R
@@ -0,0 +1,25 @@
+x <-c(3,12,7)
+x
+y<-c(4,9,11)
+y
+z<- x+y
+z
+x<-x+2
+x
+y<-y+50
+y
+sum(x)
+mean(y)
+w<-c(2,7,NA,20)
+w
+sum(w)
+v<- sum(w,na.rm = TRUE)
+v
+typeof(v)
+x>y
+x<-append(x,12)
+x
+u<-c(5:20)
+u
+u<-append(u,letters[6:10])
+u
